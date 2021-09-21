c561cdd5574c6bdfe62a64bb30e442a8ea4c1c5d
clde-ber <clde-ber@student.42.fr>
2021-09-21 11:09:16 +0200

    phonebook

---
+++ phonebook.cpp
@@ -0,0 +1,23 @@
+#include "phonebook.h"
+
+using namespace std;
+
+int catch_option(string line)
+{
+    try
+    {
+        
+
+    }
+
+}
+
+int main(void)
+{
+    string  line;
+
+    while (getline(cin, line))
+    {
+        catch_option(line);
+    }
+}
